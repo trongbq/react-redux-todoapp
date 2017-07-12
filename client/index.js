a1c92acab78ca7db1beb09ec31258c41a452a60b
Trong Bui <trong.buiquoc@gmail.com>
2017-07-12 23:26:30 +0700

    Using connect for container component, extract action creators

--- client/index.js
+++ client/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { createStore, combineReducers } from "redux";
 import { Provider } from "react-redux";
+import { connect } from "react-redux";
 
 const todo = (state, action) => {
   switch(action.type) {
@@ -53,6 +54,29 @@ const todoApp = combineReducers({
   visibilityFilter
 });
 
+let nextTodoId = 0;
+const addTodo = (text) => {
+  return {
+    type: "ADD_TODO",
+    id: nextTodoId++,
+    text: text
+  };
+};
+
+const toogleTodo = (id) => {
+  return {
+    type: "TOGGLE_TODO",
+    id: id
+  };
+};
+
+const setVisibilityFilter = (filter) => {
+  return {
+    type: "SET_VISIBILITY_FILTER",
+    filter: filter
+  };
+};
+
 // Presentational Component
 const Link = ({
   active,
@@ -76,42 +100,22 @@ const Link = ({
   );
 };
 
-// Container component
-class FilterLink extends React.Component {
-  componentDidMount() {
-    this.unsubscribe = this.context.store.subscribe(() => {
-      this.forceUpdate();
-    });
+const mapStateToLinkProps = (state, ownProps) => {
+  return {
+    active: ownProps.filter === state.visibilityFilter
   }
-
-  componentWillUnmount() {
-    this.unsubscribe();
+};
+const mapDispatchToLinkProps = (dispatch, ownProps) => {
+  return {
+    onClick: () => {
+      dispatch(setVisibilityFilter(ownProps.filter));
+    }
   }
-
-  render() {
-    const props = this.props;
-    const store = this.context.store;
-    const state = store.getState();
-
-    return (
-      <Link
-        active={props.filter === state.visibilityFilter}
-        onClick={() => {
-          store.dispatch({
-            type: "SET_VISIBILITY_FILTER",
-            filter: props.filter
-          })
-        }}
-      >
-        {props.children}
-      </Link>
-    )
-  }
-}
-
-FilterLink.contextTypes = {
-  store: React.PropTypes.object
-}
+};
+const FilterLink = connect(
+  mapStateToLinkProps,
+  mapDispatchToLinkProps
+)(Link);
 
 // Presentational Component
 const Todo = ({
@@ -146,18 +150,14 @@ const TodoList = ({
 );
 
 // Presentational Component
-const AddTodo = (props, { store }) => {
+let AddTodo = ({ dispatch }) => {
   let input;
 
   return (
     <div>
       <input ref={node => input = node} />
       <button onClick={() => {
-        store.dispatch({
-          type: "ADD_TODO",
-          text: input.value,
-          id: nextTodoId++
-        });
+        dispatch(addTodo(input.value));
         input.value = "";
       }}>
         Add Todo
@@ -165,10 +165,7 @@ const AddTodo = (props, { store }) => {
     </div>
   );
 };
-
-AddTodo.contextTypes = {
-  store: React.PropTypes.object
-}
+AddTodo = connect()(AddTodo);
 
 // Presentational Component
 const Footer = (props, { store }) => (
@@ -195,40 +192,22 @@ const getVisibleTodos = (todos, filter) => {
   }
 };
 
-class VisibleTodoList extends React.Component {
-  componentDidMount() {
-    this.unsubscribe = this.context.store.subscribe(() => {
-      this.forceUpdate();
-    });
+const mapStateToTodoListProps = (state) => {
+  return {
+    todos: getVisibleTodos(state.todos, state.visibilityFilter)
   }
-
-  componentWillUnmount() {
-    this.unsubscribe();
-  }
-
-  render() {
-    const { store } = this.context;
-    const state = store.getState();
-
-    return (
-      <TodoList
-        todos={ getVisibleTodos(state.todos, state.visibilityFilter) }
-        onTodoClick={(id) => {
-          store.dispatch({
-            type: "TOGGLE_TODO",
-            id: id
-          });
-        }}
-      />
-    );
+};
+const mapDispatchToTodoListProps = (dispatch) => {
+  return {
+    onTodoClick: (id) => {
+      dispatch(toogleTodo(id));
+    }
   }
 }
-
-VisibleTodoList.contextTypes = {
-  store: React.PropTypes.object
-}
-
-let nextTodoId = 0;
+const VisibleTodoList = connect(
+  mapStateToTodoListProps,
+  mapDispatchToTodoListProps
+)(TodoList);
 
 const TodoApp = ({ store }) => (
   <div>
