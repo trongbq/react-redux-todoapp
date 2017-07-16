e6c6dc0d7029f8a10faf9319030a5a64cad95be3
Trong Bui <trong.buiquoc@gmail.com>
2017-07-16 22:22:17 +0700

    Update data with fetched data for each tab

--- client/reducers/index.js
+++ client/reducers/index.js
@@ -1,10 +1,25 @@
 import { combineReducers } from "redux";
-import todos, * as fromTodos from "./todos";
+import byId, * as fromById from "./byId";
+import createList, * as fromList from "./createList";
 
-const todoApp = combineReducers({
-  todos
+const allIds = createList("all");
+const activeIds = createList("active");
+const completedIds = createList("completed");
+
+const listByFilter = combineReducers({
+  all: allIds,
+  active: activeIds,
+  completed: completedIds,
 });
 
-export default todoApp;
+const todos = combineReducers({
+  byId,
+  listByFilter
+});
 
-export const getVisibleTodos = (state, filter) => fromTodos.getVisibleTodos(state.todos, filter);
+export default todos;
+
+export const getVisibleTodos = (state, filter) => {
+  const ids = fromList.getIds(state.listByFilter[filter]);
+  return ids.map(id => fromById.getTodo(state.byId, id));
+};
